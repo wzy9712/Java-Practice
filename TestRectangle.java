8298f2e15f00778f87160088654c757e8db79fd6
wzy9712 <56664630+wzy9712@users.noreply.github.com>
2019-11-28 22:22:33 +0800

    Add files via upload

---
+++ TestRectangle.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class TestRectangle {
+
+	public static void main(String[] args) {
+		//æÿ–Œ≤‚ ‘¿‡
+		Rectangle o1 = new Rectangle();
+		o1.setHeight(2);
+		o1.setWidth(3);
+		System.out.println("o1 Perimeter:"+o1.getPerimeter());
+		System.out.println("o1 Area:     "+o1.getArea());
+		System.out.println("o1 Height:   "+o1.getHeight());
+		System.out.println("o1 Width:    "+o1.getWidth());
+	
+	}
+}
