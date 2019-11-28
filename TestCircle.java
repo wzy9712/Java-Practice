ea169566fd7ce769caf305ee4811a8da68707014
wzy9712 <56664630+wzy9712@users.noreply.github.com>
2019-11-28 22:17:22 +0800

    Create TestCircle.java

---
+++ TestCircle.java
@@ -0,0 +1,11 @@
+package Practice;
+
+public class TestCircle {
+	public static void main(String[] args) {
+			Circle o1 = new Circle();
+			o1.setRadius(2);
+			System.out.println("o1的半径是"+o1.getRadius());
+			System.out.println("o1的面积是"+o1.getArea());
+			System.out.println("o1的周长是"+o1.getPerimeter());
+	}
+}
