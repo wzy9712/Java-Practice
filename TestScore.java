2d7e2a26b1d0eb5acfde1e425a478c993406dacf
wzy9712 <56664630+wzy9712@users.noreply.github.com>
2019-11-29 20:49:49 +0800

    Add files via upload

---
+++ TestScore.java
@@ -0,0 +1,27 @@
+package Practice;
+import java.util.Scanner;
+public class TestScore {
+
+	private static Scanner reader;
+
+	public static void main(String[] args) {
+		reader = new Scanner(System.in);
+		System.out.print("请输入成绩");
+		double Score = reader.nextDouble();
+		if(Score > 100 || Score < 0) {
+			System.out.println("成绩不合法");
+		}else if(Score >= 90) {
+			System.out.println("成绩等级为A");
+		}else if(Score >=  80) {
+			System.out.println("成绩等级为B");
+		}else if(Score >= 70) {
+			System.out.println("成绩等级为C");
+		}else if(Score >= 60) {
+			System.out.println("成绩等级为D");
+		}else{
+			System.out.println("成绩等级为E");
+		
+		}
+	}
+
+}
