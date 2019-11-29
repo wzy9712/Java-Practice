1f2c600ba6b56bb48ce258170c133d858e2609ec
wzy9712 <56664630+wzy9712@users.noreply.github.com>
2019-11-29 21:08:53 +0800

    Add files via upload

---
+++ HanShu.java
@@ -0,0 +1,14 @@
+package Practice;
+
+public class F {
+		public static double f(double x) {
+			if(x < 1) {
+				return x;
+			}else if(x < 10) {
+				return (2*x-1);
+			}else {
+				return (3*x-11);
+			}
+	}
+	}
+
