e910ff13588965985227b024893377488e56cf45
wzy9712 <56664630+wzy9712@users.noreply.github.com>
2019-12-01 21:46:05 +0800

    Rename TestPoint.java to 12月/TestPoint.java

--- 12月/TestPoint.java
+++ 12月/TestPoint.java
@@ -3,11 +3,11 @@ package Practice;
 public class TestPoint {
 
 	public static void main(String[] args) {
-		//测试点类
+		//娴嬭瘯鐐圭被
 		Point p1 =new Point(0,0);
 		Point p2 =new Point(3,4);
 		System.out.println(p1.Show());
 		System.out.println(p2.Show());
-		System.out.println("两个点的距离是："+p1.distance(p2));
+		System.out.println("涓や釜鐐圭殑璺濈鏄細"+p1.distance(p2));
 	}
 }
